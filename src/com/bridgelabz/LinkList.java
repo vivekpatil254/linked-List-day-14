929894b4860e9d67a5d11cdaadd1bbb63978b83e
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 <Vivekpatil254@gmail.com>
2022-05-10 10:37:40 +0530

    UC1 Commit

---
+++ src/com/bridgelabz/LinkList.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+
+public class LinkList {
+    Node head;
+
+    public static void main(String[] args) {
+        LinkList linkList = new LinkList();
+        linkList.add(56);
+        linkList.add(30);
+        linkList.add(70);
+        linkList.print();
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+        public class Node {
+            int data;
+            Node next;
+
+       public Node(int data)
+       {
+           this.data=data;
+       }
+
+}
