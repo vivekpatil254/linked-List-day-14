12f35e17b6c76dadd961933154427a0107086c07
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 <Vivekpatil254@gmail.com>
2022-05-21 15:27:05 +0530

    UC6 commit

--- src/com/bridgelabz/LinkList.java
+++ src/com/bridgelabz/LinkList.java
@@ -6,57 +6,80 @@ public class LinkList {
     public static void main(String[] args) {
         LinkList linkList = new LinkList();
 
-        linkList.addAtEnd(56);
-        linkList.addAtEnd(70);
-        linkList.addBetween(30);
-        linkList.pop();
+
+        linkList.add(70);
+        linkList.add(30);
+        linkList.add(56);
+        linkList.popLast();
+        //linkList.addBetween(70);
+        //linkList.pop();
         linkList.print();
     }
-    public void pop()
-    {
-        Node deleteElement = head;
-        head=head.next;
-        deleteElement.data=0;
-        deleteElement.next=null;
+
+    public void popLast() {
+        if (head == null)
+            System.out.println("the linklist is empty");
+        else {
+            Node deleteElement = head;
+
+            Node temp=deleteElement;
+            while (deleteElement.next != null) {
+                temp =deleteElement;
+                deleteElement=deleteElement.next;
+            }
+            temp.next=null;
+            deleteElement.data=0;
+            deleteElement.next=null;
+            System.out.println("last element popped out sucessfully ...");
+
+
+        }
+
+
     }
-    public void addBetween(int data)
-    {
+
+
+    public void pop() {
+        Node deleteElement = head;
+        head = head.next;
+        deleteElement.data = 0;
+        deleteElement.next = null;
+    }
+
+    public void addBetween(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
         } else {
-            Node temp=head;
-            while(temp.next!=null)
-            {
-                if(temp.data==56)
-                {
-                    newNode.next=temp.next;
-                    temp.next=newNode;
+            Node temp = head;
+            while (temp.next != null) {
+                if (temp.data == 56) {
+                    newNode.next = temp.next;
+                    temp.next = newNode;
                     break;
                 }
-                temp=temp.next;
+                temp = temp.next;
             }
 
         }
 
 
-
     }
-    public void addAtEnd(int data)
-    {
+
+    public void addAtEnd(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
         } else {
-            Node temp=head;
-            while(temp.next!=null)
-            {
-                temp=temp.next;
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            temp.next=newNode;
-            newNode.next=null;
+            temp.next = newNode;
+            newNode.next = null;
         }
     }
+
     public void add(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -70,7 +93,7 @@ public class LinkList {
     public void print() {
         Node temp = head;
         while (temp != null) {
-            System.out.println(temp.data+"  ===>>  ");
+            System.out.println(temp.data + "  ===>>  ");
             temp = temp.next;
         }
     }
--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -9,4 +9,5 @@ package com.bridgelabz;
            this.data=data;
        }
 
+
 }
